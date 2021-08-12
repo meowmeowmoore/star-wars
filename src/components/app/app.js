3f0f362cb27f217859684709c401de5fe859a840
Nina <78531442+meowmeowmoore@users.noreply.github.com>
2021-08-12 19:30:11 +0500

    added the ability to refresh the planet after refreshing the page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import Header from '../header/';
 import RandomPlanet from '../random-planet';
 import ItemList from '../item-list';
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,4 +1,4 @@
-class SwapiService {
+export default class SwapiService {
 
   _apiBase = 'https://swapi.dev/api';
 
@@ -24,15 +24,3 @@ class SwapiService {
     return this.getResource(`/starships/${id}`);
   }
 }
-
-const swapi = new SwapiService();
-
-swapi.getStarship(3).then((p) => {
-  console.log(p.name)
-})
-
-// swapi.getAllPeople().then((people) => {
-//   people.forEach((p) => {
-//     console.log(p.name)
-//   })
-// })
